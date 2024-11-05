0f0d37e1882244334b4bbee794994c81b15ae568
Sammo <116154150+sammo-2000@users.noreply.github.com>
2024-11-05 15:49:22 +0000

    Shadcn

---
+++ frontend/src/app/plan/page.tsx
@@ -0,0 +1,11 @@
+import "server-only";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Pick a plan",
+  description: "Pick a plan that suits your needs",
+};
+
+export default async function PlanPage() {
+  return <></>;
+}
