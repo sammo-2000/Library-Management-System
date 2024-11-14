f63af8d88a1f00aa51e1a0b19fb30c9dc0c24329
nayan-builds <c2020083@hallam.shu.ac.uk>
2024-11-14 10:06:37 +0000

    Docker and run-services update

--- run-services.sh
+++ run-services.sh
@@ -19,3 +19,17 @@ echo "PORT=\"3005\"" >> .env
 echo "DATABASE_URL=file:./borrowing.db" >> .env
 docker compose up -d
 cd ../
+
+cd inventory
+if [ -f .env ]; then
+    rm .env
+fi
+echo "PORT=\"3003\"" >> .env
+echo "DB_PORT=\"5001\"" >> .env
+echo "DB_HOST=\"localhost\"" >> .env
+echo "DB_USER=\"myUser\"" >> .env
+echo "DB_PASSWORD=\"myPassword\"" >> .env
+echo "DB_NAME=\"inventoryDB\"" >> .env
+docker compose docker-compose.inventory.db.yml up -d
+docker compose up -d
+cd ../
