6104b5e584b013150b342a27723dbb8556ef1f29
Ahmed <c1032994@hallam.shu.ac.uk>
2024-12-04 17:36:30 +0000

    added auth check to reservation page

--- frontend/src/app/reservation/page.tsx
+++ frontend/src/app/reservation/page.tsx
@@ -1,7 +1,11 @@
 import ReservationLogic from "./component/reservation";
+import { getUser } from "@/functions/auth/getUser";
+import { redirect } from "next/navigation";
 
 export default async function ReservationPage() {
- 
+  const user = await getUser();
+  if (!user) redirect("/login");
+
   return (  
      <div>
       <ReservationLogic/>
