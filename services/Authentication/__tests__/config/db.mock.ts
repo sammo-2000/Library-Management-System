eb478b0d71f111bb4511d87980c05ec9660aae92
Ahmed <c1032994@hallam.shu.ac.uk>
2024-12-15 23:38:48 +0000

    auth unit tests added

--- services/Authentication/__tests__/config/db.mock.ts
+++
@@ -1,9 +0,0 @@
-// tests/config/db.mock.ts
-const mockQuery = jest.fn();
-
-const mockPool = {
-  query: mockQuery, // Attach the mock to the query method
-};
-
-export default mockPool;
-export { mockQuery }; // Export the mock for further use in tests
