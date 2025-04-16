b73dadf118da0242f9ac411df2d517809a7ebc18
Sammo <116154150+sammo-2000@users.noreply.github.com>
2025-04-16 19:01:27 +0100

    Fixed issue with session management

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,6 +1,8 @@
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 import { getUserInfo } from "./api/auth/getId";
+import { deleteToken } from "./functions/auth/deleteToken";
+import { getToken } from "./functions/auth/getToken";
 
 const unauthOnlyRoutes = ["/login", "/sign-up"];
 const authOnlyRoutes = ["/sessions"];
@@ -12,6 +14,11 @@ function matches(pathname: string, routes: string[]) {
 export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
   const user = await getUserInfo();
+
+  if (!user) {
+    const token = await getToken();
+    if (token) await deleteToken();
+  }
 
   // Unauthenticated-only pages (login/signup)
   if (matches(pathname, unauthOnlyRoutes)) {
